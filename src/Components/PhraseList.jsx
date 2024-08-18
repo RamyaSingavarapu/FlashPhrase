bd1f0f05977c3aa35d39cec34cbe97f62c013ab6
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-08-18 12:20:49 +0200

    Fix styling for delete button

--- src/Components/PhraseList.jsx
+++ src/Components/PhraseList.jsx
@@ -10,9 +10,9 @@ function PhraseList({ list, onDeletePhrase }) {
                 <Box style={{ backgroundColor: "rgb(232,234,238)" }} sx={{ display: "grid", rowGap: 2, p: 2 }}>
                     {list.map((phrase, index) => {
                         return (<Card key={index}>
-                            <CardContent>
-                                {phrase}
-                                <button onClick={() => onDeletePhrase(index)}>delete</button>
+                            <CardContent sx={{ display: "grid", gridTemplateColumns: "4fr 1fr" }}>
+                                <span style={{ marginTop: "10px" }}>{phrase}</span>
+                                <button onClick={() => onDeletePhrase(index)}>Delete</button>
                             </CardContent>
                         </Card>)
                     })}
